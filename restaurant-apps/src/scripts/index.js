6114983e88f4fa470cd730b3cef8f12906d11a55
dwihp2 <m.dwiheryanto2@gmail.com>
2021-05-07 00:25:42 +0700

    adding raw post section

--- restaurant-apps/src/scripts/index.js
+++ restaurant-apps/src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 
+
 const menu = document.querySelector('#menu');
 const hero = document.querySelector('.hero');
 const main = document.querySelector('main');

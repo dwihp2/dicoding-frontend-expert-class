2ea33895a14f28c5ce30951361bf277f616625d4
Guntur Kurniawan Heryanto <guntur.kurniawan@webimp.com.sg>
2021-05-07 22:01:53 +0700

    first commit

---
+++ restaurant-apps/src/scripts/index.js
@@ -0,0 +1,26 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+
+const data = require('../DATA.json');
+const app = document.querySelector('#app');
+    app.innerHTML = `<pre>${JSON.stringify(data, null, 4)}</pre>`;
+
+
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener("click", event=> {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener("click", ()=> {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener("click", ()=> {
+    drawer.remove('open');
+});

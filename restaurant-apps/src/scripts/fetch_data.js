b3727c6d84503211ff715e06c12aa5cc133dd318
dwihp2 <m.dwiheryanto2@gmail.com>
2021-05-08 18:34:33 +0700

    edit function&html appenddata to card

--- restaurant-apps/src/scripts/fetch_data.js
+++ restaurant-apps/src/scripts/fetch_data.js
@@ -3,38 +3,27 @@ const restaurantData = require('../DATA.json');
 
 export function appendData() {
     // dummy test response from DATA.json
-    const app = document.querySelector('#app');
-    app.innerHTML = `<pre>${JSON.stringify(restaurantData.restaurants[0], null, 4)}</pre>`;
+    // const app = document.querySelector('#app');
+    // app.innerHTML = `<pre>${JSON.stringify(restaurantData.resto.length, null, 4)}</pre>`;
 
-    let cardData = document.getElementById("card-data");
-    // for (let i =0; i < restaurantData.restaurants.length; i++) {        
-    //     let cardItem = document.createElement("article"); 
-    //     cardItem.className = "post-item";
-    //     cardItem.innerHTML = `
-    //         <img class="post-item__thumbnail"
-    //             src=${restaurantData.restaurants[i].pictureId}
-    //             alt="Test alternative img description">
-    //         <div class="post-item__content">
-    //             <p class="post-item__date">About 10 Hours Ago By <a href="#" class="post-item__date__author">Dicoding
-    //                 Intern</a>
-    //             </p>
-    //             <h1 class="post-item__title"><a href="#">${restaurantData.restaurants[i].pictureId}</a></h1>
-    //             <p class="post-item__description">test description</p>
-    //         </div>
-    //     `
-    // }
-
-    cardData.appendChild(cardItem);
+    for (let i =0; i < restaurantData.resto.length; i++) {  
+        let cardData = document.querySelector("#card-data");
+        let cardItem = document.createElement("article"); 
+        cardItem.className = "post-item";
+        cardItem.innerHTML = `
+            <div class="post-item__content_header">
+                <img class="post-item__thumbnail"
+                    src="${restaurantData.resto[i].pictureId}"
+                    alt="Gambar dua orang wanita sedang memegang laptop">
+                <div class="post-item__location">${restaurantData.resto[i].city}</div> 
+            </div>
+            <div class="post-item__content_main">
+                <p class="post-item__rating">Rating: ${restaurantData.resto[i].rating}</p>
+                <h1 class="post-item__title"><a href="#">${restaurantData.resto[i].name}</a></h1>
+                <p class="post-item__description">${restaurantData.resto[i].description}</p>
+            </div>
+        `
+        cardData.appendChild(cardItem);
+    }
+    
 }
-
-
-
-// function appendData(restaurantData) {
-//     for (let i = 0; i < restaurantData.restaurants.length; i++) {
-//         var cardData = document.getElementById("cardData");
-//         let cardItem = document.createElement("article");
-//         cardItem.className = "post-item";
-//         cardItem.innerHTML = ;
-//         cardData.appendChild(cardItem);
-//     }
-// }

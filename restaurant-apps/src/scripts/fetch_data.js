c6642fc203967a06e34f65bd663829ce5c055356
dwihp2 <m.dwiheryanto2@gmail.com>
2021-05-10 19:54:21 +0700

    fetching restaurants data(from the dummy data)

--- restaurant-apps/src/scripts/fetch_data.js
+++ restaurant-apps/src/scripts/fetch_data.js
@@ -6,21 +6,21 @@ export function appendData() {
     // const app = document.querySelector('#app');
     // app.innerHTML = `<pre>${JSON.stringify(restaurantData.resto.length, null, 4)}</pre>`;
 
-    for (let i =0; i < restaurantData.resto.length; i++) {  
+    for (let i =0; i < restaurantData.restaurants.length; i++) {  
         let cardData = document.querySelector("#card-data");
         let cardItem = document.createElement("article"); 
         cardItem.className = "post-item";
         cardItem.innerHTML = `
             <div class="post-item__content_header">
                 <img class="post-item__thumbnail"
-                    src="${restaurantData.resto[i].pictureId}"
-                    alt="Gambar dua orang wanita sedang memegang laptop">
-                <div class="post-item__location">${restaurantData.resto[i].city}</div> 
+                    src="${restaurantData.restaurants[i].pictureId}"
+                    alt="${restaurantData.restaurants[i].altName}">
+                <div class="post-item__location">${restaurantData.restaurants[i].city}</div> 
             </div>
             <div class="post-item__content_main">
-                <p class="post-item__rating">Rating: ${restaurantData.resto[i].rating}</p>
-                <h1 class="post-item__title"><a href="#">${restaurantData.resto[i].name}</a></h1>
-                <p class="post-item__description">${restaurantData.resto[i].description}</p>
+                <p class="post-item__rating">Rating: ⭐${restaurantData.restaurants[i].rating}</p>
+                <h1 class="post-item__title"><a href="#">${restaurantData.restaurants[i].name}</a></h1>
+                <p class="post-item__description">${restaurantData.restaurants[i].description}</p>
             </div>
         `
         cardData.appendChild(cardItem);
